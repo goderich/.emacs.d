2ea5de9a9311e475830723e7b237226003eee71f
Goderich <yuhsien77@gmail.com>
2025-04-06 22:12:58 +0800

    Add preliminary typst support in pandoc.el

--- pandoc.el
+++ pandoc.el
@@ -3,10 +3,18 @@
 ;; Elisp functions to call Pandoc from Emacs
 (require 'f)
 
-(defun pandoc--defaults-option (extension)
-  (pcase extension
-    ("pdf" "-dpdf")
-    ("html" "-drev")
+(defun pandoc--extension (format)
+  (pcase format
+    ("latex" "pdf")
+    ("typst" "pdf")
+    ("revealjs" "html")
+    ("docx" "docx")))
+
+(defun pandoc--defaults-option (format)
+  (pcase format
+    ("latex" "-dpdf")
+    ("typst" "-dtypst")
+    ("revealjs" "-drev")
     ("docx" "-ddoc")))
 
 (defun pandoc--find-csl (dir)
@@ -24,56 +32,68 @@
       (-> input (f-no-ext) (s-concat "-handout." extension))
     (f-swap-ext input extension)))
 
-(cl-defun pandoc-org--convert (&key extension self-contained numbered handout empty)
+(cl-defun pandoc-org--convert (&key format self-contained numbered handout empty)
   "Convert the current file using pandoc.
 The format and the defaults file need to be supplied by the caller."
   (save-buffer)
-  (let* ((args (pandoc-org--get-args extension self-contained numbered handout empty)))
+  (let* ((args (pandoc-org--get-args format self-contained numbered handout empty)))
     (message "Calling: %s" args)
     (set-process-sentinel
      (apply #'start-process "pandoc" "*pandoc*" args)
      #'pandoc-process-sentinel)))
 
-(defun pandoc-org--get-args (extension self-contained numbered handout empty)
+(defun pandoc-org--get-args (format self-contained numbered handout empty)
   "Helper function to construct the correct pandoc call."
   (let* ((input (f-this-file))
          (dir (f-dirname input))
+         (extension (pandoc--extension format))
          (output (pandoc--output-name input extension handout))
-         (defaults (pandoc--defaults-option extension))
+         (defaults (pandoc--defaults-option format))
          (metadata (f-join dir "metadata.yaml"))
          (style? (f-exists? (f-join dir "style.css")))
          (csl (pandoc--find-csl dir)))
     `("pandoc" ,input ,defaults
       ,@(when (f-exists? metadata) `("--metadata-file" ,metadata))
       ;; Temporary bugfix for https://github.com/jgm/pandoc/issues/6431
-      ,@(when (string= extension "html")
+      ,@(when (string= format "revealjs")
           '("-V" "revealjs-url=https://unpkg.com/reveal.js@^4"))
-      ,@(when (and (string= extension "html") style?)
+      ,@(when (and (string= format "revealjs") style?)
           '("--css" "./style.css"))
-      ,@(when (string= extension "html")
+      ,@(when (string= format "revealjs")
           (if handout
               '("--metadata=handout" "--incremental=false")
             '("--incremental=true")))
-      ,@(when (and (string= extension "html") self-contained)
+      ,@(when (and (string= format "revealjs") self-contained)
           '("--embed-resources=true" "--standalone"))
       ,@(when (and (member extension '("pdf" "docx")) numbered)
           '("--number-sections"))
-      ,@(when (and (string= extension "pdf") empty)
+      ,@(when (and (string= format "latex") empty)
           '("--variable=pagestyle:empty"))
-      "--csl" ,csl
+      ;; Temporarily disable CSL styles in typst due to bug
+      ,@(unless (string= format "typst")
+          `("--csl" ,csl))
       "-o" ,output)))
 
 (defvar pandoc-org->pdf-hook nil
   "Hook to run before converting from org-mode to PDF.")
 
-(defun pandoc-org->pdf ()
+(defun pandoc-org->pdf-latex ()
   "Convert the current file to pdf using pandoc.
 Works only on org files using my pdf template."
   (interactive)
   (run-hooks 'pandoc-org->pdf-hook)
   (let ((num? (transient-arg-value "number-sections" (transient-args 'pandoc-transient)))
         (empty? (transient-arg-value "empty" (transient-args 'pandoc-transient))))
-    (pandoc-org--convert :extension "pdf" :numbered num? :empty empty?)))
+    (pandoc-org--convert :format "latex" :numbered num? :empty empty?)))
+
+(defun pandoc-org->pdf-typst ()
+  "Convert the current file to pdf using pandoc.
+Works only on org files using my pdf template."
+  (interactive)
+  (run-hooks 'pandoc-org->pdf-hook)
+  (let ((num? (transient-arg-value "number-sections" (transient-args 'pandoc-transient)))
+        (empty? (transient-arg-value "empty" (transient-args 'pandoc-transient))))
+    (pandoc-org--convert :format "typst" :numbered num? :empty empty?)))
 
 (defun pandoc-org->revealjs ()
   "Convert the current file to revealjs using pandoc.
@@ -81,13 +101,13 @@ Works only on org files using my revealjs template."
   (interactive)
   (let ((handout? (transient-arg-value "handout" (transient-args 'pandoc-transient)))
         (self-con? (transient-arg-value "self-contained" (transient-args 'pandoc-transient))))
-    (pandoc-org--convert :extension "html" :handout handout? :self-contained self-con?)))
+    (pandoc-org--convert :format "revealjs" :handout handout? :self-contained self-con?)))
 
 (defun pandoc-org->docx ()
   "Convert the current file to pdf using pandoc.
 Works only on org files using my docx template."
   (interactive)
-  (pandoc-org--convert :extension "docx"))
+  (pandoc-org--convert :format "docx"))
 
 (transient-define-infix pandoc--handout? ()
   :argument "handout"
@@ -121,9 +141,14 @@ Works only on org files using my docx template."
   :init-value (lambda (obj)
                 (oset obj value nil)))
 
+;; TODO: rearrange command order,
+;; so that format is chosen first, then options.
+;; This way I can control which options are
+;; available with which formats.
 (transient-define-prefix pandoc-transient ()
   ["Convert this file with pandoc..."
-   [("p" "to pdf" pandoc-org->pdf)
+   [("p" "to pdf (LaTeX)" pandoc-org->pdf-latex)
+    ("t" "to pdf (typst)" pandoc-org->pdf-typst)
     ("r" "to revealjs" pandoc-org->revealjs)
     ("d" "to docx" pandoc-org->docx)]
    [("q" "quit" transient-quit-all)]]
